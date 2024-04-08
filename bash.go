6ec4cb5d09640132e048e0fcb4469e0df26b2461
ynurmakh <nur_erbol_2002@mail.ru>
2024-04-08 21:10:05 +0600

    bash file name hashed

--- bash.go
+++ bash.go
@@ -1,6 +1,8 @@
 package gouseful
 
 import (
+	"crypto/sha512"
+	"encoding/hex"
 	"errors"
 	"os"
 	"os/exec"
@@ -19,17 +21,38 @@ func Bash(cmdString_or_cmdsStrArr interface{}) (string, error) {
 	}
 	cmd = "# !/bin/bash\n\n" + cmd
 
-	os.Remove("tempCommands.sh")
-	defer os.Remove("tempCommands.sh")
-	err := os.WriteFile("tempCommands.sh", []byte(cmd), 0777)
+	name := getName(cmd)
+
+	os.Remove(name)
+
+	defer os.Remove(name)
+	err := os.WriteFile(name, []byte(cmd), 0777)
 	if err != nil {
 		return "", err
 	}
 
-	outByte, err := exec.Command("bash", "tempCommands.sh").CombinedOutput()
+	outByte, err := exec.Command("bash", name).CombinedOutput()
 	if err != nil {
 		return "", err
 	}
 
 	return string(outByte), nil
+}
+
+func getName(command string) string {
+	hash := sha512.New()
+
+	hash.Write([]byte(command))
+	hashed := hash.Sum(nil)
+	for256Name := hex.EncodeToString(hashed)
+
+	nameBytes := hash.Sum([]byte(for256Name))
+
+	namestring := hex.EncodeToString(nameBytes)
+
+	if len(namestring) > 252 {
+		namestring = namestring[:252]
+	}
+
+	return namestring + ".sh"
 }
